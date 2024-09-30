97c6f934dc651bf971c9d66ba474187c25bc7658
ganjos <gmmanjos@gmail.com>
2024-09-30 20:01:39 -0300

    Projeto_Blog - Criando exemplos de variaveis por parametros de requisição
    Exemplo de Loop com foreach e condicional no EJS

--- blog/index.js
+++ blog/index.js
@@ -3,9 +3,23 @@ const app     = express();
 
 app.set('view engine','ejs'); // Fazendo o Express utilizar a ferramenta EJS como View Engine
 
-app.get("/",function(req,res){
-
-    res.send("Bem vindo ao Blog!");
+app.get("/:nome?/:ling?",function(req,res){
+    var nome = req.params.nome;
+    var ling = req.params.ling;
+    
+    var exibirMsg = false;
+    var perguntas  = [
+        {questao: "Qual seu nome?", resposta: "Gabriel", solicitante: "Jeferson" },
+        {questao: "Qual sua idade?",resposta: "22" ,solicitante: "João"}
+    ]; 
+    
+    res.render("index",{
+        nome: nome,
+        ling: ling,
+        msg : exibirMsg,
+        perguntas: perguntas
+ 
+    });
 
 });
 
