d84db32f54c3aa90c91aa03722a9d2415f590eb6
ganjos <gmmanjos@gmail.com>
2024-09-30 21:48:54 -0300

    Projeto_Blog - Adicionando telas estáticas junto com css

--- blog/index.js
+++ blog/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app     = express();
 
 app.set('view engine','ejs'); // Fazendo o Express utilizar a ferramenta EJS como View Engine
+app.use(express.static('public')); // 
 
 app.get("/:nome?/:ling?",function(req,res){
     var nome = req.params.nome;
