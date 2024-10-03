7f157d4f4f8656f21e4a671028f2d71f4828cd4d
ganjos <gmmanjos@gmail.com>
2024-10-02 21:41:11 -0300

    Projeto_Blog - Adicionando o framework Bootstrap

--- blog/index.js
+++ blog/index.js
@@ -4,25 +4,32 @@ const app     = express();
 app.set('view engine','ejs'); // Fazendo o Express utilizar a ferramenta EJS como View Engine
 app.use(express.static('public')); // 
 
-app.get("/:nome?/:ling?",function(req,res){
-    var nome = req.params.nome;
-    var ling = req.params.ling;
-    
-    var exibirMsg = false;
-    var perguntas  = [
-        {questao: "Qual seu nome?", resposta: "Gabriel", solicitante: "Jeferson" },
-        {questao: "Qual sua idade?",resposta: "22" ,solicitante: "João"}
-    ]; 
-    
-    res.render("index",{
-        nome: nome,
-        ling: ling,
-        msg : exibirMsg,
-        perguntas: perguntas
- 
-    });
 
-});
+app.get("/",(req,res) => {
+
+    res.render("index")
+
+}); 
+
+// app.get("/:nome?/:ling?",function(req,res){
+//     var nome = req.params.nome;
+//     var ling = req.params.ling;
+    
+//     var exibirMsg = false;
+//     var perguntas  = [
+//         {questao: "Qual seu nome?", resposta: "Gabriel", solicitante: "Jeferson" },
+//         {questao: "Qual sua idade?",resposta: "22" ,solicitante: "João"}
+//     ]; 
+    
+//     res.render("index",{
+//         nome: nome,
+//         ling: ling,
+//         msg : exibirMsg,
+//         perguntas: perguntas
+ 
+//     });
+
+// });
 
 app.listen(8080,function(){
      console.log("App Executando")
