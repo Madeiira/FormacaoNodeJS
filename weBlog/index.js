334d38005688d2400c2e516601ec376206642c32
ganjos <gmmanjos@gmail.com>
2024-10-08 20:11:47 -0300

    Weblog - Criando base do novo projeto
    Adicionado conexão ao banco, criado a primeira rota padrão web '/' adicionado também o bootstrap

---
+++ weBlog/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { USE } = require('sequelize/lib/index-hints');
+const bodyParser = require('body-parser');
+const connection = require('./database/database')
+
+const app = express()
+const port = 8080
+
+//view engine
+app.set('view engine', 'ejs');
+
+//Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//Static Archives
+app.use(express.static('public'))
+
+// Database authentication
+
+connection 
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso")
+    }).catch((error) =>{
+        console.log(error)
+    });
+ 
+ 
+app.get("/",(req,res) =>{
+    res.render("index");
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta: ${port}`)
+});
+
+
