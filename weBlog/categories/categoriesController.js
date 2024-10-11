37b157da47c06fd26d9723625f8ad55cf89f7db8
ganjos <gmmanjos@gmail.com>
2024-10-11 19:35:39 -0300

    Weblog - Desenvolvendo Front-end para o formulario de criação de novas categorias

--- weBlog/categories/categoriesController.js
+++ weBlog/categories/categoriesController.js
@@ -5,8 +5,8 @@ router.get('/categories', (req, res) => {
   res.send('Route categories')
 });
 
-router.post('/admin/categories/new', function (req, res) {
-  res.send('POST create new categories')
+router.get('/admin/categories/new', function (req, res) {
+  res.render("admin/categories/New")
 })
 
 module.exports = router;
