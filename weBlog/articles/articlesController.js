970c8948e28e85fe12aad1f649c1c7f19451ce1b
ganjos <gmmanjos@gmail.com>
2024-10-14 20:01:11 -0300

    Weblog - Adicionando listagem de categorias para linkar ao artigo

--- weBlog/articles/articlesController.js
+++ weBlog/articles/articlesController.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router  = express.Router();
+const category = require("../categories/Category")
 
 router.get('/articles', (req, res) => {
   res.send('Route Articles')
 });
 
 router.get('/admin/articles/new', function (req, res) {
-  res.render('admin/articles/new')
+  category.findAll().then(categories =>{
+
+    res.render('admin/articles/new',{categories:categories})
+
+  })
 })
 
 module.exports = router;
