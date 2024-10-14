feea849dab42625b4a0dec994412c502a186e551
ganjos <gmmanjos@gmail.com>
2024-10-14 18:22:06 -0300

    Weblog - Desenvovlendo front end para cadastro de novos artigos pt 2

--- weBlog/articles/articlesController.js
+++ weBlog/articles/articlesController.js
@@ -5,8 +5,8 @@ router.get('/articles', (req, res) => {
   res.send('Route Articles')
 });
 
-router.post('/admin/articles/new', function (req, res) {
-  res.send('POST create new articles')
+router.get('/admin/articles/new', function (req, res) {
+  res.render('admin/articles/new')
 })
 
 module.exports = router;
