4cda3a7fc50cd7d97701ce30a857b4c2ffefd0b1
ganjos <gmmanjos@gmail.com>
2024-10-22 18:19:59 -0300

    Weblog - Ajustando timezone do banco de dados

--- weBlog/database/database.js
+++ weBlog/database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize")
 const connection = new Sequelize('weblog', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '-03:00'
 })
 
 module.exports = connection;
